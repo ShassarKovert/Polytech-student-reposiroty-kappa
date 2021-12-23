b917ac2223a35413d69bf2e4e7367de86d4a8a86
ShassarKovert <65663689+ShassarKovert@users.noreply.github.com>
2021-12-23 10:59:12 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,102 +1,178 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	system("chcp 1251");
+//	system("cls");
+//	int a, b, vsp, c;
+//	printf("Блок 1\nЭта программа присваивает переменным большее значние, если они не равны, или делает их нулями\nВведите значение первой переменной: ");
+//	scanf_s("%d", &a);
+//	printf("Введите значение второй переменной: ");
+//	scanf_s("%d", &b);
+//	if (a != b)
+//	{
+//		if (a > b)
+//			b = a;
+//		else
+//			a = b;
+//	}
+//	else
+//	{
+//		a = 0;
+//		b = 0;
+//	}
+//	printf("Новые значения переменных:\nА = %d\nВ = %d", a, b);
+//	return 0;
+//}
 
-int main()
-{
-	system("chcp 1251");
-	system("cls");
-	float a, b = 1024, c, d;
-	int e, g, h;
-	e = 0;
-	g = 0;
-	printf("Блок 1\nПрограмма считает количество килобайт, требуемое для хранения энного количества байт\n");
-	printf("Введите количество байт: ");
-	scanf_s("%f", &a);
-	c = a / b;
-	if ((int) a % (int)b != 0)
-		e= (int)c+1;
-	g = (int)c;
-	if (e == g)
-		printf("Необходимо %d кб", g);
-	if (e > g)
-		printf("Необходимо %d кб", e);
-	else
-		printf("Необходимо %d кб", g);
-	return 0;
-}*/
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	system("chcp 1251");
+//	system("cls");
+//	int a, b, d, c;
+//	printf("Блок 2\nЭта программа находит сумму двух наибольших переменных\nВведите значение первой переменной: ");
+//	scanf_s("%d", &a);
+//	printf("Введите значение второй переменной: ");
+//	scanf_s("%d", &b);
+//	printf("Введите значение третьей переменной: ");
+//	scanf_s("%d", &c);
+//	if ((a < b) && (a < c))
+//		d = b + c;
+//	if ((b < a) && (b < c))
+//		d = a + c;
+//	if ((c < a) && (c < b))
+//		d = a + b;
+//	printf("%d - сумма переменных", d);
+//	return 0;
+//}
 
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	system("chcp 1251");
+//	system("cls");
+//	float ax, ay, bx, by, cx, cy, d, e;
+//	printf("Блок 3\nЭта программа определяет, какая из первых двух точек в пространстве ближе к третьей\nВведите координату х точки A: ");
+//	scanf_s("%f", &ax);
+//	printf("Введите значение у точки A: ");
+//	scanf_s("%f", &ay);
+//	printf("Введите значение х точки B: ");
+//	scanf_s("%f", &bx);
+//	printf("Введите значение у точки B: ");
+//	scanf_s("%f", &by);
+//	printf("Введите значение х точки C: ");
+//	scanf_s("%f", &cx);
+//	printf("Введите значение у точки C: ");
+//	scanf_s("%f", &cy);
+//	d = sqrtf(((bx - cx) * (bx - cx)) + ((by - cy) * (by - cy)));
+//	e = sqrtf(((ax - cx) * (ax - cx)) + ((ay - cy) * (ay - cy)));
+//	if (d > e)
+//		printf("Точка В ближе к точке С, расстояние - %f ", d);
+//	if (e > d)
+//		printf("Точка А ближе к точке С, расстояние - %f", e);
+//	else
+//		printf("Точки находятся на равном расстоянии от точки С");
+//	return 0;
+//}
 
-int main()
-{
-	system("chcp 1251");
-	system("cls");
-	int c;
-	float a, b, d;
-	printf("Блок 2\nЭта программа считает количество отрезков\nВведите длину отрезка А: ");
-	scanf_s("%f", &a);
-	printf("Введите длину В: ");
-	scanf_s("%f", &b);
-	d = a / b;
-	c = (int)d;
-	printf("Вот столько: %d", c);
-	return 0;
-}*/
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int main()
-{
-	system("chcp 1251");
-	system("cls");
-	float a, b, c;
-	printf("Блок 3\nЭта программа считает остаток от вычитания всех чисел из числа...\nВведите число А: ");
-	scanf_s("%f", &a);
-	printf("Введите число В: ");
-	scanf_s("%f", &b);
-	while (a > b)
-		a -= b;
-	printf("%f - вот столько останется", a);
-	return 0;
-}*/
-
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int main()
-{
-	system("chcp 1251");
-	system("cls");
-	int d, e, a;
-	printf("Блок 4\nЭта программа меняет местами цифры двузначного числа\nВведите число: ");
-	scanf_s("%d", &a);
-	d = a % 10;
-	e = a / 10;
-	printf("%d%d - вот такое получилось число, красиво?", d, e);
-	return 0;
-}*/
-
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main()
 {
 	system("chcp 1251");
 	system("cls");
-	int a, b, c, d, e;
-	printf("f\nf\nf: ");
-	scanf_s("%d", &a);
-	e = a / 100;
-	c = a % 100;
-	b = c / 10;
-	d = c % 10;
-	printf("%d%d%d", b, d, e);
+	float ax, ay;
+	printf("Блок 4\nЭта программа определяет номер координатной четверти, в которой лежит точка\nВведите координату х точки: ");
+	scanf_s("%f", &ax);
+	printf("Введите координату у точки: ");
+	scanf_s("%f", &ay);
+	if ((ax > 0) && (ay > 0))
+		printf("Точка лежит в 1 четверти");
+	if ((ax < 0) && (ay > 0))
+		printf("Точка лежит во 2 четверти");
+	if ((ax < 0) && (ay < 0))
+		printf("Точка лежит в 3 четверти");
+	if ((ax > 0) && (ay < 0))
+		printf("Точка лежит в 4 четверти");
 	return 0;
 }
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	system("chcp 1251");
+//	system("cls");
+//	int a;
+//	printf("Блок 5\nЭта программа выводит описание данной двузначной переменной\nВведите значение переменной: ");
+//	scanf_s("%d", &a);
+//	if (a == 0)
+//		printf("Это нулевое число");
+//	if ((a > 0) && (a % 2 == 0))
+//		printf("Это четное положительное число");
+//	if ((a > 0) && (a % 2 != 0))
+//		printf("Это нечетное положительное число");
+//	if ((a < 0) && (a % 2 == 0))
+//		printf("Это четное отрицательное число");
+//	if ((a < 0) && (a % 2 != 0))
+//		printf("Это нечетное отрицательное число");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	system("chcp 1251");
+//	system("cls");
+//	int a;
+//	printf("Блок 6\nЭта программа выводит описание числа в диапазоне от 1 до 999\nВведите значение переменной: ");
+//	scanf_s("%d", &a);
+//	if (a == 0)
+//		printf("Это нулевое число");
+//	if ((a % 2 == 0) && (a < 10)&&(a!=0))
+//		printf("Это четное однозначное число");
+//	if ((a % 2 == 0) && (a > 9) && (a < 100))
+//		printf("Это четное двузначное число");
+//	if ((a % 2 == 0) && (a > 99) && (a < 1000))
+//		printf("Это четное трехзначное число");
+//	if ((a % 2 != 0) && (a < 10))
+//		printf("Это нечетное однозначное число");
+//	if ((a % 2 != 0) && (a > 9) && (a < 100))
+//		printf("Это нечетное двузначное число");
+//	if ((a % 2 != 0) && (a > 99) && (a < 1000))
+//		printf("Это нечетное трехзначное число");
+//	if (a > 999)
+//		printf("Слишком большое число");
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int z = 0;
+//	for(int i=1;i<=3;i++)
+//	{
+//		z = z + 2 * i;
+//	}
+//	printf("%d", z);
+//	return 0;
+//}
